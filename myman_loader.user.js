78811d7b9f6510aa83b5e613ee54180a0dd05d16
PanosGK <55203881+PanosGK@users.noreply.github.com>
2026-07-20 04:14:28 +0300

    Update myman_loader.user.js

--- myman_loader.user.js
+++ myman_loader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MyManager All-in-One Suite
 // @namespace    http://tampermonkey.net/
-// @version      33
+// @version      32
 // @description  An all-in-one suite for mymanager.gr. Auto-updates from GitHub — install this file once.
 // @author       Gkorogias
 // @match        *://thefixers.mymanager.gr/*
